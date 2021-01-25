1cc6069726d168e6e4d6de0ea71cf26fb6bc87ed
Hüseyin Topcu <77989471+harmonicamele@users.noreply.github.com>
2021-01-26 01:04:41 +0300

    Intialize Project

---
+++ Flapy/Assets/Script/Birdy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdy : MonoBehaviour
+{
+    public bool isDead;   
+    public float velocity = 1f;
+    public Rigidbody2D Rgbd;
+    public GameManager ManegerGame;
+
+    public GameObject DeadScreen;
+
+   
+    void Start()
+    {
+        
+        Time.timeScale = 1; 
+
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))   
+        {
+            Rgbd.velocity = Vector2.up * velocity; 
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)   
+    {
+        if (collision.gameObject.name == "ScoreArea")
+        {
+            ManegerGame.UpdateScore(); 
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)  
+    {
+        if (collision.gameObject.tag == "DeadArea")
+        {
+            isDead = true;
+            Time.timeScale = 0;
+            DeadScreen.SetActive(true);
+        }
+    }
+}
---
+++ Flapy/Assets/Script/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour  
+{
+    public Birdy Birdscrept;
+
+    public GameObject Boru;
+    public float Yukseklik;
+
+    public float time;
+    void Start()
+    {
+        StartCoroutine(Spawnobj(time));    
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public IEnumerator Spawnobj (float time)  
+    {
+        while (!Birdscrept.isDead )    
+        {
+
+            Instantiate(Boru, new Vector3(1.5f, Random.Range(-Yukseklik, Yukseklik), 0), Quaternion.identity);
+             
+            yield return new WaitForSeconds(time);   
+
+            
+        }
+            
+        
+    }
+}
+
